0835f96e031d8c48ec3b051803752f17fbca3e3f
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-03-21 01:55:26 -0400

    Create initial HTML and js to make ajax call

---
+++ assets/javascript/app-1.js
@@ -0,0 +1,33 @@
+// Global delcarations
+
+var apiKey = "YHnPcJvXmqkIrQvG5kkUeG4dfTIaCOel";
+
+var queryURL;
+
+queryURL = "https://api.giphy.com/v1/gifs/search?api_key=YHnPcJvXmqkIrQvG5kkUeG4dfTIaCOel&q=&limit=25&offset=0&rating=G&lang=en";
+
+var giphyresponse = {};
+
+// jQuery wrapper
+$(document).ready(function () {
+    console.log("ready!");
+
+
+    $("#button1").on("click", function () {
+        // Perfoming an AJAX GET request to our queryURL
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+
+            // After the data from the AJAX request comes back
+            .then(function (response) {
+                giphyresponse = Object.assign({}, response);
+                console.log(giphyresponse);
+
+            });
+    });
+
+
+
+});
